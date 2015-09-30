4bd35ab3b29a6436f888e0273d24b7aedcc7845c
Dom Batten <dominic.batten@googlemail.com>
2015-09-30 14:06:34 +0100

    Temperature showing, working on increase temp button

--- src/application.js
+++ src/application.js
@@ -1,7 +1,28 @@
-  $( document ).ready(function(){
-    $( "a" ).click(function( event ) {
-      alert( "Come back soon" );
-      event.preventDefault();
-      $( this ).hide( "slow" );
-    });
-  });
+$( document ).ready(function(){
+  thermostat = new Thermostat();
+  power = new Power();
+
+  $("#temp").text(thermostat.temperature);
+
+  $("#up").click(function( thermostat.increase_temp(power) ));
+
+
+
+});
+
+
+
+//   $( document ).ready(function(){
+//     $( "a" ).click(function( event ) {
+//       alert( "Come back soon" );
+//       event.preventDefault();
+//       $( this ).hide( "slow" );
+//     });
+//   });
+
+// document.getElementById("output").innerHTML=name;
+
+// function somefunction(){
+//   var somevalue = "Data to be inserted";
+//   $("#SecondDiv").text(somevalue);
+// }
