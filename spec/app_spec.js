b94bb18ac6f4101c552824d67241cb03f05b203e
Lucy Fang <lucyfang2@gmail.com>
2015-10-01 11:08:13 +0100

    Amended html, completed change colors function

---
+++ spec/app_spec.js
@@ -0,0 +1,24 @@
+describe('Thermostat', function() {
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+  });
+
+  it('displays default temperature', function() {
+    expect('#temp').toContainText('20');
+  });
+
+  it('temperature text is yellow when 20 degrees', function() {
+    var $temperature = $('#temp');
+    expect($temperature.text()).toHaveCss({color: "yellow"})
+  })
+
+  it("increases temperature by 1 degree", function() {
+    // var $temperature = $('#temp');
+    // var $upLink = $('#up');
+
+    $('#up').click();
+    expect($('#temp').text()).toContainText("21");
+  });
+
+});
--- src/application.js
+++ src/application.js
@@ -1,18 +1,21 @@
-$( document ).ready(function(){
+$(document).ready(function() {
   thermostat = new Thermostat();
   power = new Power();
 
+
   $("#temp").text(thermostat.temperature);
-  $("#display_colour").text(thermostat.colour);
+  $('#temp').css('color', thermostat.colour);
 
   $("#up").click(function(){
     $("#temp").text(thermostat.increase_temp(power));
     thermostat._change_display();
-    $("#display_colour").text(thermostat.colour);
+    $('#temp').css('color', thermostat.colour);
   });
 
   $("#down").click(function(){
     $("#temp").text(thermostat.decrease_temp());
+    thermostat._change_display();
+    $('#temp').css('color', thermostat.colour);
   });
 
   $('#power_save').change(function() {
@@ -20,38 +23,12 @@ $( document ).ready(function(){
     if(thermostat.temperature > 25) {
       thermostat.temperature = 25;
       $("#temp").text(thermostat.temperature);
+      $('#temp').css('color', thermostat.colour);
     };
   });
 
   $("#reset").click(function(){
     $("#temp").text(thermostat.reset_temp());
+    $('#temp').css('color', thermostat.colour);
   });
 });
-
-
-
-
-  //  if($('#power_save').is(':checked')) {
-  //     power.switch_on();
-  //   } else {
-  //     power.switch_off();
-  //   };
-  //   });
-
-
-
-
-//   $( document ).ready(function(){
-//     $( "a" ).click(function( event ) {
-//       alert( "Come back soon" );
-//       event.preventDefault();
-//       $( this ).hide( "slow" );
-//     });
-//   });
-
-// document.getElementById("output").innerHTML=name;
-
-// function somefunction(){
-//   var somevalue = "Data to be inserted";
-//   $("#SecondDiv").text(somevalue);
-// }
