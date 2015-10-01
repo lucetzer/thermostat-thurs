fb5eeac1ce9cbebc927927a2bb4ce5a1ebf3562b
Dom Batten <dominic.batten@googlemail.com>
2015-10-01 13:10:55 +0100

    Open weather map API working with ajax and json, about to do some CSS

--- src/weather.js
+++ src/weather.js
@@ -8,14 +8,27 @@ $(document).ready(function() {
     $('.weather').html(weather);
   };
 
-  $('#cityBtn').click(function() {
+  // $('#cityBtn').click(function() {
+  //   var city = $('#city').val();
+  //   $.getJSON('http://api.openweathermap.org/data/2.5/find?q=' + city +
+  //   '&units=metric', function(outcome) {
+  //     var city = outcome.list[0].name
+  //     var temperature = Math.floor(outcome.list[0].main.temp) + ' °C';
+  //     showCity(city);
+  //     showWeather(temperature);
+  //   });
+  // });
+
+  $("#cityBtn").click(function(){
     var city = $('#city').val();
-    $.getJSON('http://api.openweathermap.org/data/2.5/find?q=' + city +
-    '&units=metric', function(outcome) {
-      var city = outcome.list[0].name
-      var temperature = Math.floor(outcome.list[0].main.temp) + ' °C';
-      showCity(city);
-      showWeather(temperature);
+    $.ajax('http://api.openweathermap.org/data/2.5/find?q=' + city + '&units=metric', {
+      success: function(data){
+        console.log(data);        
+        var location = data.list[0].name;
+        var temperature = Math.floor(data.list[0].main.temp) + ' °C';
+        showCity(location);
+        showWeather(temperature);
+      }
     });
   });
 
